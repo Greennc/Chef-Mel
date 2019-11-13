8926031af8c4ee601110fa954771746178ddd325
Nyasha <52669511+Greennc@users.noreply.github.com>
2019-11-13 15:39:48 -0500

    Chef Mel's site
    Professional catering site for chef mel

---
+++ Chef Mel's Site/JS/script.js
@@ -0,0 +1,9 @@
+
+// onscroll javascript
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $("#mainNavbar");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    });
+});
+// end of on scroll javascript
